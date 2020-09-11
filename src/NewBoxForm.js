3a2d353a3af3d8750dbae033041524ea13db29dc
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-11 15:40:35 -0400

    add two way binding with handleChange

--- src/NewBoxForm.js
+++ src/NewBoxForm.js
@@ -6,17 +6,19 @@ class NewBoxForm extends React.Component{
     height: "",
     color:"",
   }
-
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
   render(){
     return (
       <div>
         <form>
           <label>Width: </label>
-          <input name="width" type="text" value={this.state.width}/>
+          <input name="width" type="text" value={this.state.width} onChange={this.handleChange}/>
           <label>Height:  </label>
-          <input name="height" type="text" value={this.state.height}/>
+          <input name="height" type="text" value={this.state.height} onChange={this.handleChange}/>
           <label>Color: </label>
-          <input name="color" type="text" value={this.state.color}/>
+          <input name="color" type="text" value={this.state.color} onChange={this.handleChange}/>
           <button type="submit">Add Box</button>
         </form>
       </div>
