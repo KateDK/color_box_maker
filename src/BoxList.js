d10984f76a5d1340c109a2f6b723fd359af748e9
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-14 14:27:25 -0400

    Pass props to Box component

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import NewBoxForm from './NewBoxForm';
-
+import Box from './Box'
 class BoxList extends React.Component{
   state={
     colorBoxes: [],
   }
   addBox = (box) => {
-
-    console.log(">>>Box: ",box);
     this.setState({colorBoxes:[...this.state.colorBoxes,box]})
   }
   render(){
+    const {colorBoxes} = this.state;
     return (
       <div>
       <NewBoxForm addBox={this.addBox}/>
       BoxList
-
+      {colorBoxes.map(box=><Box color={box.color} width={box.width} height={box.height}/>)}
       </div>)
   }
 }
