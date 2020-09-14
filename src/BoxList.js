0386dd687707119aee1018b66db7f6f02940cbd1
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-14 14:42:59 -0400

    add keys with uuid

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import NewBoxForm from './NewBoxForm';
-import Box from './Box'
-class BoxList extends React.Component{
-  state={
+import Box from './Box';
+import { v4 as uuidv4 } from 'uuid';
+
+class BoxList extends React.Component {
+  state = {
     colorBoxes: [],
-  }
+  };
   addBox = (box) => {
-    this.setState({colorBoxes:[...this.state.colorBoxes,box]})
-  }
-  render(){
-    const {colorBoxes} = this.state;
+    this.setState({ colorBoxes: [...this.state.colorBoxes, box] });
+  };
+  render() {
+    const { colorBoxes } = this.state;
     return (
       <div>
-      <NewBoxForm addBox={this.addBox}/>
-      BoxList
-      {colorBoxes.map(box=><Box color={box.color} width={Number(box.width)} height={Number(box.height)}/>)}
-      </div>)
+        <NewBoxForm addBox={this.addBox} />
+        BoxList
+        {colorBoxes.map((box) => (
+          <Box
+            color={box.color}
+            width={Number(box.width)}
+            height={Number(box.height)}
+            key={uuidv4()}
+          />
+        ))}
+      </div>
+    );
   }
 }
 
